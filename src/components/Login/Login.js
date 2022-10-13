61ec2be540d74225fb8cde58f2bfd9366ecccfd8
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-10-13 18:22:33 -0300

    login y accesos

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
 import './Login.scss'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email:'',
+    password:''
+  })
+  const [pass, setPass] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name] : e.target.value})
+  }
+
+  function checkPassword() {
+    formData.password === 'danoneday' ? setPass(true) : setPass(false)
+  }
+
+  function validateEmail(email) { 
+    let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(email)){
+        //Email valid. Procees to test if it's from the right domain (Second argument is to check that the string ENDS with this domain, and that it doesn't just contain it)
+        if(email.indexOf("@danone.com", email.length - "@danone.com.ar".length) !== -1){
+            return true;
+        } else {
+          return false;
+        }
+    }
+}
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    checkPassword();
+    console.log("login: ", {...formData});
+    console.log("formData.email: ", formData.email);
+    if (validateEmail(formData.email)){
+      localStorage.setItem('isLoggedIn', true);
+      navigate('/event');
+    } else {
+      alert("datos incorrectos")
+    }
+    
+  }
   return (
     <div className='login__card'>
-        <form>
-            <input type='text' placeholder='Email' /> 
-            <input type='text' placeholder='Contraseña' /> 
+        <form onSubmit={handlesubmit}>
+            <input name='email' onChange={handleChange} type='text' placeholder='Email' /> 
+            <input name='password' onChange={handleChange} type='password' placeholder='Contraseña' /> 
             <button type='submit'>Ingresar</button>
         </form>
     </div>
--- src/pages/Event/Event.js
+++ src/pages/Event/Event.js
@@ -1,26 +1,33 @@
 import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 import Vimeo from '@u-wave/react-vimeo';
 import './Event.scss'
 
 const Event = () => {
+  const navigate = useNavigate();
   const cerrarSesion = () => {
-    alert("Acción Cerrar sesión");
+    localStorage.setItem('isLoggedIn', false);
+    navigate('/');
   }
   return (
-
+    <>
+    { localStorage.getItem('isLoggedIn') ? 
+    <div className='live'>
+      <h1>Evento en vivo</h1>
+      <Vimeo
+        video="348721737"
+        width={350}
+        responsive={false}
+      />
+      <button className='logout' onClick={cerrarSesion}>Cerrar sesión</button>
+    </div>
+    :
     <div className='live'>
       <h1>Para visualizar el vivo debes estar conectado</h1>
-      <button className='login' onClick={cerrarSesion}>Iniciar sesión</button>
+      <button className='login'><Link to={'/'}>Iniciar sesión</Link></button>
     </div>
-    // <div className='live'>
-    //   <h1>Evento en vivo</h1>
-    //   <Vimeo
-    //     video="348721737"
-    //     width={350}
-    //     responsive={false}
-    //   />
-    //   <button className='logout' onClick={cerrarSesion}>Cerrar sesión</button>
-    // </div>
+    }
+    </>
   )
 }
 
